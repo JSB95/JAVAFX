28525bfe16ed6ed595e7e27914560beb87185d8b
YONG JUN KIM@matebook <godoklife1@gmail.com>
2022-04-19 22:40:30 +0900

    a

--- 3/src/controller/webview/Web.java
+++ 3/src/controller/webview/Web.java
@@ -1,7 +1,13 @@
 package controller.webview;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,10 +20,10 @@ import javafx.scene.web.WebView;
 public class Web implements Initializable{
 	
     @FXML
-    private TextField txturl;
+    private TextField txtsearch;
 
     @FXML
-    private Button btnconnect;
+    private Button btnsearch;
 
     @FXML
     private Button btnhome;
@@ -26,31 +32,67 @@ public class Web implements Initializable{
     private WebView mywebview;
 
     @FXML
-    void accconnect(ActionEvent event) {
-    	
-    	loadpage();
+    void accsearch(ActionEvent event) {
+    	String search = txtsearch.getText();
+    	txtsearch.setText("");
+    	loadpage(search);
     }
 
     @FXML
     void acchome(ActionEvent event) {
     	engine.load(getClass().getResource("/googlemap.html").toString());
-    }
+    	}
     
     private WebEngine engine;
     
-    public void loadpage() {
+    private File file = new File("c:\\adb\\searchdata.json");
+    
+    public void loadpage(String search) {
 //    	engine.load(getClass().getResource("/googlemap.html").toString());	// 로컬 주소 호출용
-    	engine.load("http://"+txturl.getText());
+    	engine.setUserDataDirectory(file);
+    	engine.load("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+search+"&key=APIKEY");
+    	
     }
     
+
+    
+    public ArrayList<String> jsonparser(String json){
+    	try {
+			FileInputStream fileInputStream = new FileInputStream("c:\\adb\\incheon.json");
+			byte[] bytes = new byte[fileInputStream.available()];	// 읽어들인 제이슨파일 크기만큼 바이트 배열 선언
+			fileInputStream.read(bytes);
+			String test = new String(bytes);	// 바이트배열을 문자열로 변환
+//			System.out.println(test);
+			fileInputStream.close();			// fileInputStream 종료
+//			System.out.println(test.length());
+//			System.out.println("---------------------");
+			
+			
+			JSONObject jsonObject = new JSONObject(test);	//json의 객체화
+//			System.out.println(jsonObject);
+			JSONArray jsonArray = jsonObject.getJSONArray("results");	// 객체화 된 json에서 배열 추출
+			ArrayList<String> searchlist = new ArrayList<String>();
+			for (int i = 0; i < jsonArray.length(); i++) {
+		        JSONObject obj = jsonArray.getJSONObject(i);
+		        String name = obj.getString("name");	
+		        System.out.println("name(" + i + "): " + name);// 배열 i번 인덱스에서 key(name)의 value값 저장
+//		        String global_code = obj.getString("global_code");
+
+//		        System.out.println("global_code(" + i + "): " + global_code);
+		        System.out.print("\n");
+		        searchlist.add(i, name);
+		    }
+			return searchlist;
+		} catch (Exception e) {System.out.println(e);}
+    	return null;
+    }
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
     	engine = mywebview.getEngine();
-    	engine.load(getClass().getResource("/googlemap.html").toString());
+//    	engine.load(getClass().getResource("/googlemap.html").toString());
+//    	engine.load("https://www.google.com/maps/@?api=1&map_action=map&query=인천공항");
+
     }
-    
-    
-    
 }
 
 
