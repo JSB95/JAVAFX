05eb4589d70dc94fe61e9dc74c9b53d73adc42fa
jeost <100548156+jeost@users.noreply.github.com>
2022-04-22 18:37:40 +0900

    ss

--- 3/src/controller/main/Searchpage.java
+++ 3/src/controller/main/Searchpage.java
@@ -1,16 +1,12 @@
 package controller.main;
 
 import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.ResourceBundle;
 
+import dao.AplaneDao;
 import dao.RouteDao;
-import dao.TimeDao;
 import dto.Route;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -96,55 +92,42 @@ public class Searchpage implements Initializable {
     }
     
     void searchtableshow() {
-    	try {
-	    	String departure = cbbstartplace.getValue();
-	    	String destination = cbbdestination.getValue();
-	    	ObservableList<Route> searchlist = FXCollections.observableArrayList();
-	    	ObservableList<Route> slist = RouteDao.routeDao.getroute(departure, destination);
-	    	for(int i=0; i<slist.size(); i++) {
-	    		String cname = RouteDao.routeDao.getcname(slist.get(i).getAname());
-	    		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-	    		int sgmt =TimeDao.timeDao.gettime(slist.get(i).getRdeparture());
-	    		int egmt =TimeDao.timeDao.gettime(slist.get(i).getRdestination());
-	    		int h = Integer.parseInt(slist.get(i).getRflightTime().split(":")[0]);
-	    		int m = Integer.parseInt(slist.get(i).getRflightTime().split(":")[1]);
-	    		int tm = egmt - sgmt + h;
-	    		Calendar calendar = Calendar.getInstance();
-				Date date = dateFormat.parse(slist.get(i).getRdeparturedate());
-	    		calendar.setTime(date);
-	    		calendar.add(Calendar.MINUTE, m);
-	    		calendar.add(Calendar.HOUR, tm);
-	    		String etime = dateFormat.format(calendar.getTime());
-	    		Route route = new Route(
-	    				slist.get(i).getRnum(), 
-	        			slist.get(i).getAname(), 
-	        			cname, etime,
-	        			slist.get(i).getRflightTime(), 
-	        			slist.get(i).getRdeparturedate(), 
-	        			slist.get(i).getRbaseprice());
-	    		searchlist.add(route);
-	    	}
-	
-	    	TableColumn tc = searchtable.getColumns().get(0);
-	    	tc.setCellValueFactory(new PropertyValueFactory<>("rdeparture"));
-	    	
-	    	tc = searchtable.getColumns().get(1);
-	    	tc.setCellValueFactory(new PropertyValueFactory<>("aname"));
-	    	
-	    	tc = searchtable.getColumns().get(2);
-	    	tc.setCellValueFactory(new PropertyValueFactory<>("rdeparturedate"));
-	    	
-	    	tc = searchtable.getColumns().get(3);
-	    	tc.setCellValueFactory(new PropertyValueFactory<>("rdestination"));
-	    	
-	    	tc = searchtable.getColumns().get(4);
-	    	tc.setCellValueFactory(new PropertyValueFactory<>("rflightTime"));
-	    	
-	    	tc = searchtable.getColumns().get(5);
-	    	tc.setCellValueFactory(new PropertyValueFactory<>("rbaseprice"));
-	    	
-	    	searchtable.setItems(searchlist);
-    	}catch (Exception e) {}
+    	String departure = cbbstartplace.getValue();
+    	String destination = cbbdestination.getValue();
+    	ObservableList<Route> searchlist = FXCollections.observableArrayList();
+    	ObservableList<Route> slist = RouteDao.routeDao.getroute(departure, destination);
+    	for(int i=0; i<slist.size(); i++) {
+    		String cname = RouteDao.routeDao.getcname(slist.get(i).getAname());
+    		Route route = new Route(
+    				slist.get(i).getRnum(), 
+        			slist.get(i).getAname(), 
+        			cname, 
+        			"µµÂø½Ã°£", 
+        			slist.get(i).getRflightTime(), 
+        			slist.get(i).getRdeparturedate(), 
+        			slist.get(i).getRbaseprice());
+    		searchlist.add(route);
+    	}
+
+    	TableColumn tc = searchtable.getColumns().get(0);
+    	tc.setCellValueFactory(new PropertyValueFactory<>("rdeparture"));
+    	
+    	tc = searchtable.getColumns().get(1);
+    	tc.setCellValueFactory(new PropertyValueFactory<>("aname"));
+    	
+    	tc = searchtable.getColumns().get(2);
+    	tc.setCellValueFactory(new PropertyValueFactory<>("rdeparturedate"));
+    	
+    	tc = searchtable.getColumns().get(3);
+    	tc.setCellValueFactory(new PropertyValueFactory<>("rdestination"));
+    	
+    	tc = searchtable.getColumns().get(4);
+    	tc.setCellValueFactory(new PropertyValueFactory<>("rflightTime"));
+    	
+    	tc = searchtable.getColumns().get(5);
+    	tc.setCellValueFactory(new PropertyValueFactory<>("rbaseprice"));
+    	
+    	searchtable.setItems(searchlist);
     }
 	
 }
