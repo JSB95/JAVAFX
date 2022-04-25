ed2da6b10ce407cde9d1d60ba147613ef43ceba9
JSB95 <97193122+JSB95@users.noreply.github.com>
2022-04-25 17:40:02 +0900

    Revert "Merge pull request #9 from JSB95/KYJ"
    This reverts commit 221014e23323a9e0e0432a8c7df0a138e28c03d4, reversing
    changes made to 96a6bc1dd420b208becf88340776e612cd3beb2e.

--- 3/src/application/Main.java
+++ 3/src/application/Main.java
@@ -1,22 +1,20 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/view/mainpage/Mainpage.fxml"));
-			Scene scene = new Scene(parent);
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("Å×½ºÆ®");
 			primaryStage.show();
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
