8d7dfee8c198d6436cc7a236da7bccc389267240
JSB95 <97193122+JSB95@users.noreply.github.com>
2022-04-25 17:57:12 +0900

    ?
    4234

--- 3/src/controller/board/Boardwrite.java
+++ 3/src/controller/board/Boardwrite.java
@@ -1,5 +1,6 @@
 package controller.board;
 
+<<<<<<< HEAD
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -17,10 +18,20 @@ import dao.BoardDao;
 import dto.Board;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+=======
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+import controller.login.Login;
+import controller.main.Mainpage;
+import dao.BoardDao;
+import dto.Board;
+>>>>>>> parent of 0c620d7 (board.fxml)
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+<<<<<<< HEAD
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
@@ -121,10 +132,64 @@ public class Boardwrite implements Initializable{
     		Alert alert = new Alert(AlertType.INFORMATION);
     		alert.setTitle("알림");
     		alert.setHeaderText("검색어를 입력 해 주세요.");
+=======
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Boardwrite implements Initializable {
+	
+    @FXML
+    private Button btnwrite;
+
+    @FXML
+    private TextField txttitle;
+
+    @FXML
+    private TextArea txtcontent;
+
+    @FXML
+    private Button btnback;
+
+    @FXML
+    void back(ActionEvent event) {
+    	Mainpage.instance.loadpage("/view/board/board.fxml");
+    }
+
+    @FXML
+    void write(ActionEvent event) { //  게시물 등록 버튼을 눌렀을때 이벤트 
+    	// 1. 컨트롤에 입력된 데이터 가져오기
+    	String title = txttitle.getText(); // 해당 fxid객체에서 입력된 내용물 가져오기
+    	String content = txtcontent.getText();
+    		// * 작성자 : 현재 로그인된 객체는 login 클래스내 member객체에 저장되어 있음
+    	String write = Login.member.getMid();
+    	
+    	// 2. 객체화 [ 번호(생략=자동) , 제목 , 내용 , 작성자 , 작성일(생략=자동) , 조회수(생략=자동) ]
+    	Board board = new Board( 0 , title , content , write , null , 0);
+    	// 3. db저장
+    		// 1. new 객체를 이용한 메소드 호출 
+	//    	BoardDao boardDao = new BoardDao();
+	//    	boardDao.write(board);
+    		// 2. 클래스내 static 변수 호출후 메소드 호출 
+    	boolean result =  BoardDao.boardDao.write(board);
+    	if( result ) {
+    		// 메시지
+    		Alert alert = new Alert( AlertType.INFORMATION );
+    			alert.setHeaderText("게시글이 작성 되었습니다.");
+    		alert.showAndWait();
+    		// 페이지 전환
+        	Mainpage.instance.loadpage("/view/board/board.fxml");
+    	}else {
+    		// 메시지
+    		Alert alert = new Alert( AlertType.INFORMATION );
+    			alert.setHeaderText("게시글 작성 실패[관리자에게문의]");
+>>>>>>> parent of 0c620d7 (board.fxml)
     		alert.showAndWait();
     	}
     }
     
+<<<<<<< HEAD
     String key = controller.webview.Gmaps.gmaps.getAPIkey();	// 내 노트북 파일로 들어있는 구글맵스 api 키
     @FXML
     void accwrite(ActionEvent event) {
@@ -242,4 +307,26 @@ public class Boardwrite implements Initializable{
     	mapsearchfield=null;
     		// 글 쓰다 말고 뒤로가기 했을때 메모리에 쓰래기값 남아있을수 있으니 실행때마다 초기화
     }
+=======
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+    	
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+>>>>>>> parent of 0c620d7 (board.fxml)
 }
--- 3/src/dao/ReplyDao.java
+++ 3/src/dao/ReplyDao.java
@@ -1,9 +1,18 @@
 package dao;
 
+<<<<<<< HEAD
+=======
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import controller.login.Login;
+>>>>>>> parent of 0c620d7 (board.fxml)
 import dto.Reply;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+<<<<<<< HEAD
 public class ReplyDao extends Dao{
 	
 	public static ReplyDao replyDao = new ReplyDao();
@@ -94,3 +103,81 @@ public class ReplyDao extends Dao{
 		} catch (Exception e) {System.out.println("ReplyDao_idsearchforreplytable_Exception : "+e);}
 	}
 }
+=======
+public class ReplyDao extends Dao {
+	
+	public static ReplyDao replyDao = new ReplyDao();
+	// 댓글 작성 메소드
+	public boolean wirte(Reply reply) {
+		try {
+			String sql = "insert into test.reply(rcontent,rwrite,bnum) values(?,?,?)";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, reply.getRcontent());
+			ps.setString(2, reply.getRwrite());
+			ps.setInt(3, reply.getBnum());
+			ps.executeUpdate();
+			return true;
+		} catch(Exception e) {
+			System.out.println("댓글쓰기 SQL 오류 : "+e);
+		}
+		return false;
+		
+	}
+	
+	// 댓글 불러오기 메소드
+	public ObservableList<Reply> list() {
+		ObservableList<Reply> replylist = FXCollections.observableArrayList();
+		try {
+			String sql = "select * from  test.reply";
+			ps=con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				if(rs.getInt(5)==controller.board.Board.board.getBnum()) {
+					Reply reply = new Reply(
+							rs.getInt(1), 
+							rs.getString(2), 
+							rs.getString(3), 
+							rs.getString(4), 
+							rs.getInt(5));
+					replylist.add(reply);
+				}
+			}
+			return replylist;
+		} catch(Exception e) {
+			System.out.println("댓글 불러오기 SQL 오류 : "+e);
+		}
+		return null;
+	}
+	
+	// 댓글 수정 메소드
+	public boolean replyupdate(int rnum, String rcontent) {
+		try {
+			String sql = "update  test.reply set rcontent=? where rnum=?";
+			ps = con.prepareStatement(sql);
+			ps.setString(1, rcontent);
+			ps.setInt(2, rnum);
+			ps.executeUpdate();
+			return true;
+		} catch(Exception e) {
+			System.out.println("댓글 수정 SQL 오류 : "+e);
+		}
+		return false;
+	}
+	
+	// 댓글 삭제 메소드
+	public boolean replydelete(int rnum) {
+		try {
+			String sql = "delete from  test.reply where rnum=?";
+			ps = con.prepareStatement(sql);
+			ps.setInt(1, rnum);
+			ps.executeUpdate();
+			return true;
+		} catch(Exception e) {
+			System.out.println("댓글삭제 SQL 오류 : "+e);
+		}
+		
+		return false;
+	}
+	
+}
+>>>>>>> parent of 0c620d7 (board.fxml)
