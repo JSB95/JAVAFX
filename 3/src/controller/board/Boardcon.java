d27f8f2aee1253f353ccc8882f4311e0468eab08
YONG JUN KIM@matebook <godoklife1@gmail.com>
2022-04-27 11:24:44 +0900

    조회수 정렬 부분 수정

--- 3/src/controller/board/Boardcon.java
+++ 3/src/controller/board/Boardcon.java
@@ -131,13 +131,15 @@ public class Boardcon implements Initializable{
     		boards[i] = new Board(0, 0, null, null, null, null, null, null, null, 0);
     	}
     	
-    	for(int i=0; i<list.size(); i++) {
+    	
+    	for(int i=0; i<list.size()-1; i++) {
     		for(int j=i+1; j<list.size(); j++) {
-    			if(list.get(i).getBview()<list.get(j).getBview()) {
-    				if(boards[0].getBview()<list.get(j).getBview()) 
-    					boards[0] = list.get(j);
-    				if(boards[1].getBview()<list.get(i).getBview()) 
-    					boards[1] = list.get(i);
+    			if(list.get(j).getBview() > list.get(i).getBview()) {
+    				if(list.get(j).getBview()>= boards[0].getBview()) boards[0]=list.get(j);
+    				else if(list.get(j).getBview() >= boards[1].getBview()) boards[1]=list.get(j);
+    			}else if(list.get(j).getBview() < list.get(i).getBview()) {
+    				if(list.get(i).getBview()>= boards[0].getBview()) boards[0]=list.get(i);
+    				else if(list.get(i).getBview() >= boards[1].getBview()) boards[1]=list.get(i);
     			}
     		}
     	}
